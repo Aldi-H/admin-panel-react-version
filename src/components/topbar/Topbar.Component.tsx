ec33772866d998faa0e3a3b217770f0ac294c5df
Aldi-H <aldihimawan88@gmail.com>
2024-02-19 10:36:15 +0700

    Feat: Add Topbar and TopbarLinks components

---
+++ src/components/topbar/Topbar.Component.tsx
@@ -0,0 +1,67 @@
+import Link from "next/link";
+
+import { useSidebar } from "@/context/Sidebar.Context";
+
+import TopbarLinks from "./components/TopbarLinks.Component";
+import { Separator } from "../ui/separator";
+
+interface NavbarProps {
+  currentPageRoute?: string;
+}
+
+const Topbar = (props: NavbarProps) => {
+  const { currentPageRoute } = props;
+
+  const { isCollapsed } = useSidebar();
+
+  return (
+    <div
+      className={`bg-background fixed backdrop-filter backdrop-blur-lg bg-opacity-30 border-b-[1.5px] border-secondary rounded-xl leading-6 flex min-h-20 xl:justify-center xl:items-center mx-auto mt-0 pb-2 right-3 md:right-7 lg:right-7 xl:right-10 ps-5 pt-2 top-3 md:top-4 xl:top-5 
+      ${
+        isCollapsed
+          ? "md:w-[calc(100vw-21.5%)] lg:w-[calc(100vw-17%)] xl:w-[calc(100vw-240px)]"
+          : "md:w-[calc(100vw-13.5%)] lg:w-[calc(100vw-6%)] xl:w-[calc(100vw-160px)]"
+      } 
+      `}
+    >
+      <div className="w-full flex sm:flex-col md:flex-row items-center mb-0 justify-between">
+        <div className="sm:mb-2 md:mb-0">
+          <Link href={`${currentPageRoute}`} className="font-bold text-lg ">
+            {currentPageRoute?.charAt(0).toUpperCase() +
+              (currentPageRoute?.slice(1) ?? "")}
+          </Link>
+
+          <div className="flex flex-row items-center text-sm">
+            <div>
+              <Link href="#">
+                <span>Pages</span>
+              </Link>
+            </div>
+            <Separator
+              orientation="horizontal"
+              className="-rotate-[90deg] w-4 h-[1px] bg-foreground"
+            />
+            <div>
+              <Link href="#">
+                <span>
+                  {currentPageRoute?.charAt(0).toUpperCase() +
+                    (currentPageRoute?.slice(1) ?? "")}
+                </span>
+              </Link>
+            </div>
+          </div>
+        </div>
+
+        <div className="">
+          <TopbarLinks />
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Topbar;
+
+// z-10 backdrop-filter bg-background backdrop-blur-lg bg-opacity-30 border-b border-secondary
+// w-[calc(100vw-6%)] md:w-[calc(100vw-8%)] lg:w-[calc(100vw-6%)] xl:w-[calc(100vw-240px)]
+// xl:w-[calc(100vw-240px)]
---
+++ src/components/topbar/components/TopbarLinks.Component.tsx
@@ -0,0 +1,61 @@
+import { CircleUserRound, LogOut, MoreVertical, UserRound } from "lucide-react";
+
+import { Avatar, AvatarFallback, AvatarImage } from "@/components/ui/avatar";
+import {
+  Menubar,
+  MenubarContent,
+  MenubarItem,
+  MenubarMenu,
+  MenubarTrigger,
+} from "@/components/ui/menubar";
+import { ModeToggle } from "@/components/ModeToggle.Component";
+
+const TopbarLinks = () => {
+  return (
+    <div className="flex flex-row items-center sm:w-full md:w-auto">
+      <Menubar className="border-none">
+        <MenubarMenu>
+          {/* <MenubarTrigger className="p-0 relative"> */}
+          <div className="ms-0 px-0 me-2 items-center md:flex space-x-4">
+            <Avatar>
+              <AvatarImage src="https://github.com/shadcn.png" />
+              <AvatarFallback>
+                <CircleUserRound />
+              </AvatarFallback>
+            </Avatar>
+
+            <div className="flex flex-col items-start space-y-1 leading-3">
+              <div className="flex flex-row items-center space-x-2">
+                <span>Username</span>
+                <MenubarTrigger className="p-0">
+                  <MoreVertical size={16} />
+                </MenubarTrigger>
+              </div>
+              <span className="text-xs">Admin</span>
+            </div>
+          </div>
+
+          <MenubarContent
+            className="border-none mt-10 space-y-1 py-2"
+            align="end"
+          >
+            <MenubarItem className="space-x-4 p-2 font-medium">
+              <UserRound size={20} />
+              <span>Profile</span>
+            </MenubarItem>
+            <MenubarItem className="space-x-4 p-2 text-red-600 focus:text-red-600 font-medium">
+              <LogOut size={20} />
+              <span>Logout</span>
+            </MenubarItem>
+          </MenubarContent>
+        </MenubarMenu>
+      </Menubar>
+
+      <div className="pe-6 ps-2">
+        <ModeToggle />
+      </div>
+    </div>
+  );
+};
+
+export default TopbarLinks;
