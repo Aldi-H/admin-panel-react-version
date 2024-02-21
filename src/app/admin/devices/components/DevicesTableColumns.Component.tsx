82068c8caf92f2ae1e97a90d70500aef399f4a46
Aldi-H <aldihimawan88@gmail.com>
2024-02-21 14:16:10 +0700

    Feat: Add new components for DevicesTableColumns and Data-Table

---
+++ src/app/admin/devices/components/DevicesTableColumns.Component.tsx
@@ -0,0 +1,158 @@
+"use client";
+
+import { ColumnDef } from "@tanstack/react-table";
+
+import {
+  ArrowUpDown,
+  MoreHorizontal,
+  SquarePen,
+  Trash,
+  Trash2,
+} from "lucide-react";
+
+import { Button } from "@/components/ui/button";
+import { Checkbox } from "@/components/ui/checkbox";
+import {
+  DropdownMenu,
+  DropdownMenuContent,
+  DropdownMenuItem,
+  DropdownMenuLabel,
+  DropdownMenuSeparator,
+  DropdownMenuTrigger,
+} from "@/components/ui/dropdown-menu";
+
+export type Devices = {
+  userId: string;
+  masterId: string;
+  deviceId: string;
+  deviceName: string;
+};
+
+export const DeviceColumns: ColumnDef<Devices>[] = [
+  //* Select Box column
+  {
+    id: "select",
+    header: ({ table }) => {
+      return (
+        <Checkbox
+          checked={
+            table.getIsAllPageRowsSelected() ||
+            (table.getIsSomePageRowsSelected() && "indeterminate")
+          }
+          onCheckedChange={(value) => table.toggleAllPageRowsSelected(!!value)}
+          aria-label="Select All"
+        />
+      );
+    },
+    cell: ({ row }) => {
+      return (
+        <Checkbox
+          checked={row.getIsSelected()}
+          onCheckedChange={(value) => row.toggleSelected(!!value)}
+          aria-label="Select Row"
+        />
+      );
+    },
+    enableSorting: false,
+    enableHiding: false,
+  },
+
+  //* User Id Column
+  {
+    accessorKey: "userId",
+    header: ({ column }) => {
+      return (
+        <Button
+          variant="ghost"
+          onClick={() => column.toggleSorting(column.getIsSorted() === "asc")}
+        >
+          <div className="capitalize">User Id</div>
+          <ArrowUpDown className="ml-2 h-4 w-4" />
+        </Button>
+      );
+    },
+    cell: ({ row }) => {
+      return <div className="text-justify">{row.getValue("userId")}</div>;
+    },
+  },
+
+  //* Master Id Column
+  {
+    accessorKey: "masterId",
+    header: ({ column }) => {
+      return (
+        <Button
+          variant="ghost"
+          onClick={() => column.toggleSorting(column.getIsSorted() === "asc")}
+        >
+          <div className="capitalize">Master Id</div>
+          <ArrowUpDown className="ml-2 h-4 w-4" />
+        </Button>
+      );
+    },
+    cell: ({ row }) => {
+      return <div className="text-justify">{row.getValue("masterId")}</div>;
+    },
+  },
+
+  //* Device Id Column
+  {
+    accessorKey: "deviceId",
+    header: () => {
+      return <div className="capitalize">Device Id</div>;
+    },
+    cell: ({ row }) => {
+      return <div className="text-justify">{row.getValue("deviceId")}</div>;
+    },
+  },
+
+  //* Device Name Column
+  {
+    accessorKey: "deviceName",
+    header: () => {
+      return <div className="capitalize">Device Name</div>;
+    },
+    cell: ({ row }) => {
+      return <div className="text-justify">{row.getValue("deviceName")}</div>;
+    },
+  },
+
+  //* Actions Column
+  {
+    id: "actions",
+    enableHiding: false,
+    cell: ({ row }) => {
+      const devices = row.original;
+
+      return (
+        <DropdownMenu>
+          <DropdownMenuTrigger asChild>
+            <Button variant="ghost" className="h-8 w-8 p-0">
+              <span className="sr-only">Open Menu</span>
+              <MoreHorizontal className="h-4 w-4" />
+            </Button>
+          </DropdownMenuTrigger>
+
+          <DropdownMenuContent align="end" className="border-secondary">
+            <DropdownMenuLabel>Actions</DropdownMenuLabel>
+            <DropdownMenuItem
+              className="space-x-4 p-2 font-medium"
+              onClick={() => console.log(devices.deviceId)}
+            >
+              <SquarePen size={16} />
+              <span>Edit</span>
+            </DropdownMenuItem>
+            <DropdownMenuSeparator />
+            <DropdownMenuItem
+              className="space-x-4 p-2 text-red-600 focus:text-red-600 font-medium"
+              onClick={() => console.log(devices.deviceId)}
+            >
+              <Trash2 size={16} />
+              <span>Delete</span>
+            </DropdownMenuItem>
+          </DropdownMenuContent>
+        </DropdownMenu>
+      );
+    },
+  },
+];
---
+++ src/components/table/Data-Table.Component.tsx
@@ -0,0 +1,108 @@
+"use client";
+
+import { useState } from "react";
+import {
+  ColumnDef,
+  SortingState,
+  flexRender,
+  getCoreRowModel,
+  getSortedRowModel,
+  useReactTable,
+} from "@tanstack/react-table";
+
+import {
+  Table,
+  TableBody,
+  TableCell,
+  TableHead,
+  TableHeader,
+  TableRow,
+} from "../ui/table";
+
+interface DataTableProps<TData, TValue> {
+  columns: ColumnDef<TData, TValue>[];
+  data: TData[];
+}
+
+const DataTable = <TData, TValue>({
+  columns,
+  data,
+}: DataTableProps<TData, TValue>) => {
+  const [sorting, setsorting] = useState<SortingState>([]);
+
+  const table = useReactTable({
+    data,
+    columns,
+    getCoreRowModel: getCoreRowModel(),
+    onSortingChange: setsorting,
+    getSortedRowModel: getSortedRowModel(),
+    state: {
+      sorting,
+    },
+  });
+
+  return (
+    <div className="w-full">
+      <div className="rounded-md bg-background border-[1.9px] border-secondary">
+        <Table>
+          <TableHeader>
+            {table.getHeaderGroups().map((headerGroup) => (
+              <TableRow
+                key={headerGroup.id}
+                className="border-secondary border-b-2"
+              >
+                {headerGroup.headers.map((header) => {
+                  return (
+                    <TableHead key={header.id}>
+                      {header.isPlaceholder
+                        ? null
+                        : flexRender(
+                            header.column.columnDef.header,
+                            header.getContext(),
+                          )}
+                    </TableHead>
+                  );
+                })}
+              </TableRow>
+            ))}
+          </TableHeader>
+          <TableBody>
+            {table.getRowModel().rows?.length ? (
+              table.getRowModel().rows.map((row) => {
+                return (
+                  <TableRow
+                    key={row.id}
+                    data-state={row.getIsSelected() && "selected"}
+                    className="border-none"
+                  >
+                    {row.getVisibleCells().map((cell) => {
+                      return (
+                        <TableCell key={cell.id}>
+                          {flexRender(
+                            cell.column.columnDef.cell,
+                            cell.getContext(),
+                          )}
+                        </TableCell>
+                      );
+                    })}
+                  </TableRow>
+                );
+              })
+            ) : (
+              <TableRow>
+                <TableCell
+                  colSpan={columns.length}
+                  className="h-24 text-center"
+                >
+                  No Results.
+                </TableCell>
+              </TableRow>
+            )}
+          </TableBody>
+        </Table>
+      </div>
+    </div>
+  );
+};
+
+export default DataTable;
